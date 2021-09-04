44896a4f8002549bc2933863a6ca92a328704061
Nazakat341 <nazakatshehzad341@gmail.com>
2021-09-04 04:18:06 -0700

    first commit

--- routes/returns.js
+++ routes/returns.js
@@ -1,4 +1,6 @@
-const moment = require('moment');
+
+const Joi = require('joi');
+const validate = require('../middleware/validate');
 const express = require('express');
 const router = express.Router();
 const {Rental} = require('../models/rental');
@@ -6,28 +8,33 @@ const {Movie} = require('../models/movie');
 const auth = require('../middleware/auth')
 
 
-router.post('/',auth, async(req, res) => {
-if(!req.body.customerId) return res.status(400).send('customerId is not provided'); 
-if(!req.body.movieId) return res.status(400).send('movieId is not provided'); 
 
 
-const rental = await Rental.findOne({
-    'customer._id':req.body.customerId,
-    'movie._id':req.body.movieId,
-});
+router.post('/',[auth,validate(validateReturn)], async(req, res) => {
+    // const { error } = validateReturn(req.body); 
+    // if (error) return res.status(400).send(error.details[0].message);
+   const rental = await Rental.lookup(req.body.customerId,req.body.movieId);
+
 if(!rental) return res.status(404).send('Rental is not found');
 
 if(rental.dateReturned) return res.status(400).send('rental already in processed');
-rental.dateReturned = new Date();
-const rentalDays = moment().diff(rental.dateOut, 'days');
-rental.rentalFee = rentalDays * rental.movie.dailyRentalRate; 
+
+rental.return();
 await rental.save();
 
 await Movie.update({ _id: rental.movie._id},{
     $inc: { numberInStock:1}
 });
 
-return res.status(200).send(rental);
+return res.send(rental);
  
  });
+ function validateReturn(req) {
+    const schema = {
+      customerId: Joi.objectId().required(),
+      movieId: Joi.objectId().required()         ////show error through winstson
+    };
+  
+    return Joi.validate(req, schema);
+  }
  module.exports = router;
